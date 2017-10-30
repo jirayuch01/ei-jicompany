758266acecebd1ecb83cc36501b467a85cb11d82
jirayuch01 <jirayu_ch01@hotmail.com>
2017-10-30 08:14:38 +0700

    User Model
    User Model

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ var app = express();
 var port = process.env.PORT || 8080;
 var morgan = require('morgan');
 var mongoose = require('mongoose');
+var User = require('./app/models/user');
+var bodyParser = require('body-parser');
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
 mongoose.connect('mongodb://localhost:27017/ei-jicompany', { useMongoClient: true }, function (err) {
@@ -11,6 +15,26 @@ mongoose.connect('mongodb://localhost:27017/ei-jicompany', { useMongoClient: tru
         console.log('Not connected to MongoDB!' + err);
     } else {
         console.log('Successfully connected to MongoDB!');
+    }
+});
+
+app.post('/users', function (req, res) {
+    var user = new User();
+    user.username = req.body.username;
+    user.password = req.body.password;
+    user.email = req.body.email;
+    if (req.body.username == null || req.body.username == '' ||
+        req.body.password == null || req.body.password == '' ||
+        req.body.email == null || req.body.email == '') {
+        res.send('Ensure username, email and password were provided!');
+    } else {
+        user.save(function (err) {
+            if (err) {
+                res.send('Username or Email already exists!');
+            } else {
+                res.send('create user!')
+            }
+        });
     }
 });
 
