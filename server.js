230b16d07d6230c5f038a78bc4575c0ad8d154eb
jirayuch01 <jirayu_ch01@hotmail.com>
2017-11-04 10:04:49 +0700

    setting Bootstrap and Angular
    setting Bootstrap and Angular

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
-var express = require('express');
-var app = express();
-var port = process.env.PORT || 8080;
-var morgan = require('morgan');
-var mongoose = require('mongoose');
-var User = require('./app/models/user');
+var express    = require('express');
+var app        = express();
+var port       = process.env.PORT || 8080;
+var morgan     = require('morgan');
+var mongoose   = require('mongoose');
 var bodyParser = require('body-parser');
+var router     = express.Router();
+var appRoutes  = require('./app/routes/api')(router);
+var path       = require('path');
 
+app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(morgan('dev'));
+app.use(express.static(__dirname + '/public'));
+app.use('/api', appRoutes);
 
 mongoose.connect('mongodb://localhost:27017/ei-jicompany', { useMongoClient: true }, function (err) {
     if (err) {
@@ -18,24 +22,8 @@ mongoose.connect('mongodb://localhost:27017/ei-jicompany', { useMongoClient: tru
     }
 });
 
-app.post('/users', function (req, res) {
-    var user = new User();
-    user.username = req.body.username;
-    user.password = req.body.password;
-    user.email = req.body.email;
-    if (req.body.username == null || req.body.username == '' ||
-        req.body.password == null || req.body.password == '' ||
-        req.body.email == null || req.body.email == '') {
-        res.send('Ensure username, email and password were provided!');
-    } else {
-        user.save(function (err) {
-            if (err) {
-                res.send('Username or Email already exists!');
-            } else {
-                res.send('create user!')
-            }
-        });
-    }
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/public/app/views/index.html'));
 });
 
 app.listen(port, function () {
